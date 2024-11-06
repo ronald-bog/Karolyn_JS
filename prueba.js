43ce611743a6c7a6158becfa58cd55e49ce09c49
Ronald Espitia <ronald79@gmail.com>
2024-11-05 22:27:19 -0500

    Clase

---
+++ prueba.js
@@ -0,0 +1,34 @@
+console.log("hola Karolyn");
+
+// comentario linea
+
+/*
+Comentario en Bloque
+*/
+
+// * VARIABLES *
+// Declaracion
+let alumno;
+
+// Inicializacion
+let estudiante = "Karolyn";
+
+// Asignacion
+alumno = "Carlos";
+
+// Reasignacion
+estudiante = "Sofia";
+
+// * CONSTANTES *
+const pi = 3.14;
+console.log(pi);
+
+// * Notaciones , nomenclarura, convecion, estilo de nombrado *
+
+// camelCase
+let valorProductoNuevo;
+
+// snakeCase
+let valor_producto_nuevo;
+
+
